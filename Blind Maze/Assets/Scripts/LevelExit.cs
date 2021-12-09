64f0f3fb80e9123271715753f05f6d81618a25d1
Jordan S <Jordan.schofield45@gmail.com>
2021-12-09 15:06:50 +1100

    Initial commit

---
+++ Blind Maze/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using System.Reflection;
+
+public class LevelExit : MonoBehaviour
+{
+    int NLUsed = 0;
+    public int Level;
+
+    private void Start()
+    {
+        if (NLUsed >= 250)
+        {
+            EditorApplication.isPaused = true;
+        }
+        Level = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            NextLevel();
+        }
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(Level);
+        NLUsed += 1;
+        Debug.Log("the method" + MethodBase.GetCurrentMethod() + "has Been Used:" + NLUsed);
+        return;
+    }
+}
---
+++ Blind Maze/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D rigidbody;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("PlayerKill"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        rigidbody.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("death");
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
